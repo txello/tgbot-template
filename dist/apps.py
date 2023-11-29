20d8d56b26877a8b97a990b9e808f6aa22c95c81
txello <48260455+txello@users.noreply.github.com>
2023-11-29 05:58:06 +0000

    Add v1.1

--- dist/apps.py
+++ dist/apps.py
@@ -4,7 +4,8 @@ from importlib import import_module
 
 dp = Dispatcher() # Объявляем диспетчер
 
-[[getattr(dp,j).outer_middleware(getattr(import_module(dirs.middleware),i[0])()) for j in i[1]] for i in apps.middleware.items()] # Подключаем внешние миддлвари
+[[getattr(dp,j).middleware(getattr(import_module(dirs.middleware),i[0])()) for j in i[1]] for i in apps.middleware.items()] # Подключаем внутренние миддлвари
+[[getattr(dp,j).outer_middleware(getattr(import_module(dirs.middleware),i[0])()) for j in i[1]] for i in apps.outer_middleware.items()] # Подключаем внешние миддлвари
 
 dp.include_routers(*[getattr(import_module(f'{dirs.app}.{i}'),dirs.app_router) for i in apps.data]) # Получаем роутеры из папки app определённых файлов, которые записаны в settings.apps и подключаем роутеры к диспетчеру
 
